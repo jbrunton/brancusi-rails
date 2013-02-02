9e150b4a18695610d837be7cc339a8dd55912657
jbrunton <john_brunton@hotmail.co.uk>
2013-02-02 18:38:44 +0000

    fixed directory names in view generator template

--- lib/generators/brancusi/view/templates/view.rb
+++ lib/generators/brancusi/view/templates/view.rb
@@ -1,2 +1,2 @@
-<h3><%= file_name %></h3>
-<p>Find me in <b>app/views/templates/_<%= file_name %>.html.erb</b></p>
+<h3><%= class_path.join %>/<%= file_name %></h3>
+<p>Find me in <b>app/views/templates/<%= class_path.join %>/_<%= file_name %>.html.erb</b></p>
