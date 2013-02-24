e11f7a3a61458d35f9b8956c59c6280d807127cb
jbrunton <john_brunton@hotmail.co.uk>
2013-02-24 18:36:28 +0000

    fixed relative path for TMP_ROOT

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ require "generator_spec/matcher"
 
 Dir[Pathname.new(File.expand_path('../', __FILE__)).join('support/**/*.rb')].each {|f| require f}
 
-TMP_ROOT = Pathname.new(File.expand_path('../../tmp', __FILE__))
+TMP_ROOT = Pathname.new(File.expand_path('../lib/tmp', __FILE__))
 
 RSpec.configure do |config|
   config.include Helpers::FileSystem
